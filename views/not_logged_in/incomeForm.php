7a8a7c7f5b6d77220310f2a7af4a7278f5064755
andrew <andporter@gmail.com>
2015-06-30 22:08:11 -0600

    cleaned up the php and html in the getForm function

--- views/not_logged_in/incomeForm.php
+++ views/not_logged_in/incomeForm.php
@@ -25,8 +25,6 @@ if (isset($login))
     }
 }
 
-require_once("../config/db.php");
-
 function getForm($CategoryParentType, $CategoryParentOrder)
 {
     $db_connection = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
@@ -43,41 +41,41 @@ function getForm($CategoryParentType, $CategoryParentOrder)
     {
         $ResultsToReturn = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
+    ?>
 
-    echo "<div class='panel panal-content panel-primary'>";
-    echo "<div class='panel-heading'>";
-    echo "<h3 class='panel-title'>" . $ResultsToReturn[0][categoryParentOrder] . ". " . $ResultsToReturn[0][categoryParentName] . "</h3>";
-    echo "</div>";
-    echo "<div class='panel-body'>";
-    echo "<form class='form-horizontal' role='form'>";
+    <div class='panel panal-content panel-primary'>
+        <div class='panel-heading'>
+            <h3 class='panel-title'><?php echo $ResultsToReturn[0]["categoryParentOrder"] . ". " . $ResultsToReturn[0]["categoryParentName"] ?></h3>
+        </div>
+        <div class='panel-body'>
+            <form class='form-horizontal' role='form'>
+                <?php
+                foreach ($ResultsToReturn as $row)
+                {
+                    ?>
+                    <div class = 'form-group'>
+                        <label class = 'control-label col-sm-7' for = 'self_<?php echo $row["categoryOrder"] . str_replace(' ', '_', $row["categoryName"]) ?>'><?php echo $row["categoryOrder"] . '. ' . $row["categoryName"] ?></label>
+                        <div class = 'col-sm-5 input-group input-group-unstyled'>
+                            <span class='input-group-addon glyphicon glyphicon-info-sign' rel='tooltip' title='<?php echo $row["categoryHoverToolTip"] ?>'></span>
+                            <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Self $$' value='' id = 'self_<?php echo $row["categoryOrder"] . str_replace(' ', '_', $row["categoryName"]) ?>'></input>
+                            <span class='input-group-addon glyphicon glyphicon-modal-window'></span>
+                            <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Spouse $$' value='' id = 'spouse_<?php echo $row["categoryOrder"] . str_replace(' ', '_', $row["categoryName"]) ?>'></input>
+                            <span class='input-group-addon glyphicon glyphicon-modal-window'></span>
+                        </div>
+                    </div>
+                <?php } ?>
 
-    foreach ($ResultsToReturn as $row)
-    {
-        echo "<div class = 'form-group'>";
-        echo "  <label class = 'control-label col-sm-7' for = 'self_" . $row[categoryOrder] . str_replace(' ', '_', $row[categoryName]) . "'> " . $row[categoryOrder] . '. ' . $row[categoryName] . "</label>";
-        echo "  <div class = 'col-sm-5 input-group input-group-unstyled'>";
-        echo "      <span class='input-group-addon glyphicon glyphicon-info-sign' rel='tooltip' title='" . $row[categoryHoverToolTip] . "'></span>";
-        echo "      <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Self $$' value='' required data-parsley-required-message = '' id = 'self_" . $row[categoryOrder] . str_replace(' ', '_', $row[categoryName]) . "'></input>";
-        echo "      <span class='input-group-addon glyphicon glyphicon-modal-window'></span>";
-        echo "      <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Spouse $$' value='' required data-parsley-required-message = '' id = 'spouse_" . $row[categoryOrder] . str_replace(' ', '_', $row[categoryName]) . "'></input>";
-        echo "      <span class='input-group-addon glyphicon glyphicon-modal-window'></span>";
-        echo "  </div>";
-        echo "</div>";
-    }
-
-    echo "<div class='form-group'>";
-    echo "<div class='col-sm-offset-2 col-sm-10'>";
-    echo "<button type='submit' class='btn btn-primary pull-right'>Next</button>";
-    echo "</div>";
-    echo "</div>";
-    echo "</form>";
-    echo "</div>";
-    echo "</div>";
-}
-?>
+                <div class='form-group'>
+                    <div class='col-sm-offset-2 col-sm-10'>
+                        <button type='submit' class='btn btn-primary pull-right'>Next</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php } ?>
 
 <body>
-
     <div class='container theme-showcase'>
         <?php
         getForm("Income", 1);
