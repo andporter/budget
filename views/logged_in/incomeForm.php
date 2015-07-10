bc8e4a7ad2c4560eecc7d510edc3f512b3013c2a
Braden Talbot <talbotbraden@gmail.com>
2015-07-10 11:37:57 -0600

    Removed most calculator code and used separate Calculator class.

--- views/logged_in/incomeForm.php
+++ views/logged_in/incomeForm.php
@@ -1,4 +1,5 @@
 <?php
+require("views/logged_in/Calculator.php");
 
 function getForm($CategoryParentType, $CategoryParentOrder) {
     $db_connection = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
@@ -19,92 +20,84 @@ function getForm($CategoryParentType, $CategoryParentOrder) {
     $sql->bindParam(':userId', $_SESSION['user_id']);
     $sql->bindParam(':budgetId', $_SESSION['user_budgetid']);
 
-    if ($sql->execute()) 
-    {
+    if ($sql->execute()) {
         $ResultsToReturn = $sql->fetchAll(PDO::FETCH_ASSOC);
 //        print_r($ResultsToReturn);
-    } 
-    else 
-    {
+    } else {
         exit("You do not have permission to edit this budget!");
     }
     ?>
-<style>
-    #budgetTitles{
-        color: white;
-    }   
-    
-</style>
+    <style>
+        #budgetTitles{
+            color: white;
+        }   
+
+    </style>
 
     <!--<div class='panel panal-content panel-primary'>-->
-        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-            
-            <div class="panel panel-primary">
-                <div class="panel-heading" role="tab" id="headingOne">
-                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"] 
-                            ?>" aria-expanded="true" aria-controls="<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"] ?>">
-                        <h3 class='panel-title' id="budgetTitles"><?php echo $ResultsToReturn[0]["categoryParentOrder"] . ". " . $ResultsToReturn[0]["categoryParentName"] ?></h3>
-                    </a>
-                </div>
-                <div id="<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"] 
-                            ?>" aria-expanded="true" aria-controls="<?php echo $ResultsToReturn[0]["categoryParentOrder"] . 
-                                    $ResultsToReturn[0]["categoryParentType"] ?>" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                    <div class='panel-body'>
-                        <form class='form-horizontal' role='form' id='<?php echo $CategoryParentType . $CategoryParentOrder ?>'>
-                            <?php
-                            foreach ($ResultsToReturn as $row) {
-                                ?>
-                                <div class = 'form-group'>
-                                    <label class = 'control-label col-sm-5' for = 'self_<?php echo $row["categoryId"] ?>'><?php echo $row["categoryOrder"] . '. ' . $row["categoryName"] ?></label>
-                                    <div class = 'input-group input-group-unstyled'>
-                                        <div class = 'col-sm-1'>
-                                            <span class='input-group-addon glyphicon glyphicon-info-sign' rel='tooltip' title='<?php echo $row["categoryHoverToolTip"] ?>'></span>
-                                        </div>
-                                        <div class = 'col-sm-3'>
-                                            <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Self $$' value='' id = 'self_<?php echo $row["categoryId"] ?>' name = 'self_<?php echo $row["categoryId"] ?>'></input>
-                                        </div>
-                                        <div class = 'col-sm-1'>
-                                            <?php echo getCalculator($row["calculatorType"]); ?>
-                                        </div>
-                                        <div class = 'col-sm-3'>
-                                            <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Spouse $$' value='' id = 'spouse_<?php echo $row["categoryId"] ?>' name = 'spouse_<?php echo $row["categoryId"] ?>'></input>
-                                        </div>
-                                        <div class = 'col-sm-1'>
-                                            <?php echo getCalculator($row["calculatorType"]); ?>
-                                        </div>
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+        <div class="panel panel-primary">
+            <div class="panel-heading" role="tab" id="headingOne">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"]
+    ?>" aria-expanded="true" aria-controls="<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"] ?>">
+                    <h3 class='panel-title' id="budgetTitles"><?php echo $ResultsToReturn[0]["categoryParentOrder"] . ". " . $ResultsToReturn[0]["categoryParentName"] ?></h3>
+                </a>
+            </div>
+            <div id="<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"]
+    ?>" aria-expanded="true" aria-controls="<?php
+                 echo $ResultsToReturn[0]["categoryParentOrder"] .
+                 $ResultsToReturn[0]["categoryParentType"]
+                 ?>" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class='panel-body'>
+                    <form class='form-horizontal' role='form' id='<?php echo $CategoryParentType . $CategoryParentOrder ?>'>
+                        <?php
+                        foreach ($ResultsToReturn as $row) {
+                            ?>
+                            <div class = 'form-group'>
+                                <label class = 'control-label col-sm-5' for = 'self_<?php echo $row["categoryId"] ?>'><?php echo $row["categoryOrder"] . '. ' . $row["categoryName"] ?></label>
+                                <div class = 'input-group input-group-unstyled'>
+                                    <div class = 'col-sm-1'>
+                                        <span class='input-group-addon glyphicon glyphicon-info-sign' rel='tooltip' title='<?php echo $row["categoryHoverToolTip"] ?>'></span>
+                                    </div>
+                                    <div class = 'col-sm-3'>
+                                        <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Self $$' value='' id = 'self_<?php echo $row["categoryId"] ?>' name = 'self_<?php echo $row["categoryId"] ?>'></input>
+                                    </div>
+                                    <div class = 'col-sm-1'>
+                                        <?php
+                                        $x = "self_" . $row["categoryId"];
+                                        $$x = new Calculator($row["calculatorType"], $x);
+                                        echo $$x->drawCalculator();
+                                        ?>
+                                    </div>
+                                    <div class = 'col-sm-3'>
+                                        <input type = 'text' onkeypress='return isNumberKey(event);' class = 'form-control' placeholder = 'Spouse $$' value='' id = 'spouse_<?php echo $row["categoryId"] ?>' name = 'spouse_<?php echo $row["categoryId"] ?>'></input>
+                                    </div>
+                                    <div class = 'col-sm-1'>
+                                        <?php
+                                        $y = "spouse_" . $row["categoryId"];
+                                        $$y = new Calculator($row["calculatorType"], $y);
+                                        echo $$y->drawCalculator();
+                                        ?>
                                     </div>
                                 </div>
-                            <?php } ?>
-
-                            <div class='form-group'>
-                                <div class='col-sm-offset-2 col-sm-10'>
-                                    <input type="submit" value="Next" class="btn btn-primary pull-right" />
-                                </div>
                             </div>
-                        </form>
-                    </div>
+                        <?php } ?>
+
+                        <div class='form-group'>
+                            <div class='col-sm-offset-2 col-sm-10'>
+                                <input type="submit" value="Next" class="btn btn-primary pull-right" />
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-            
-     
         </div>
-<!--</div>-->
+
+
+    </div>
+    <!--</div>-->
 <?php } ?>
-
-<?php
-
-function getCalculator($calcType, $id) {
-    if ($calcType == 'MonthlyWage') {
-        echo '<span><button type="button" class="btn btn-info" id="' . $id . '" data-toggle="modal" data-target="#wageCalcModal"><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></button></span>';
-    } elseif ($calcType == 'MonthlySE') {
-        echo '<span><button type="button" class="btn btn-info" id="' . $id . '" data-toggle="modal" data-target="#seCalcModal"><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></button></span>';
-    } elseif ($calcType == 'NonMonthly') {
-        echo '<span><button type="button" class="btn btn-info" id="' . $id . '" data-toggle="modal" data-target="#nonMonthlyCalcModal"><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></button></span>';
-    } else {
-        echo '<span class="input-group-addon glyphicon glyphicon-modal-window"></span>';
-    }
-}
-?>
 
 <body>
     <div class='container theme-showcase'>
@@ -124,96 +117,6 @@ function getCalculator($calcType, $id) {
         ?>
     </div>
 
-    <!-- Wage Calculator Modal-->
-    <div class="modal fade" id="wageCalcModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Wage Calculator</h4>
-                </div>
-                <div class="modal-body">
-                    <form class="navbar-form">
-                        <div class="form-group">
-                            <input onkeypress='return isNumberKey(event);' class="form-control" id="dollars-per-hour" placeholder="$ Per Hour"> 
-                            <span>X</span> 
-                            <input onkeypress='return isNumberKey(event);' class="form-control" id="hours-per-week" placeholder="Hours Per Week">
-                        </div><button type="button" id="wageSubmit" class="btn btn-primary">Submit</button>
-                    </form>
-                    <form class="navbar-form">
-                        <div class="form-group">
-                            <input onkeypress='return isNumberKey(event);' class="form-control" id="salary" placeholder="Salary $">
-                        </div><button type="button" id="salarySubmit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div><!-- End Modal-->
-
-    <!-- Self-Employed Calculator Modal-->
-    <div class="modal fade" id="seCalcModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Self-Employment Calculator</h4>
-                </div>
-                <div class="modal-body">
-                    <form class="navbar-form">
-                        <div class="form-group">
-                            <input onkeypress='return isNumberKey(event);' class="form-control" id="typical-month-income" placeholder="Typical Month's $">
-                        </div><button type="button" id="seMonthSubmit" class="btn btn-primary">Submit</button>
-                    </form>
-                    <form class="navbar-form">
-                        <div class="form-group">
-                            <input onkeypress='return isNumberKey(event);' class="form-control" id="last-year-taxes" placeholder="Last Year's Taxes $">
-                        </div><button type="button" id="seTaxSubmit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div><!-- End Modal-->
-
-    <!-- Non-Monthly Calculator Modal -->
-    <div class="modal fade" id="nonMonthlyCalcModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel3">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Non-Monthly Calculator</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <form class="navbar-form">
-                                    <div class="form-group" style="text-align: center;">
-                                        <label class = 'control-label'>Estimate for the Year</label>
-                                        <input onkeypress='return isNumberKey(event);' class="form-control" id="estimate-for-year" placeholder="$">
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="col-sm-8">
-                                <form class="navbar-form">
-                                    <div class="form-group">
-                                        <div class="col-sm-12" style="text-align: center;"><label class = 'control-label'>Frequency Method</label></div>
-                                        <div class="col-sm-6"><label class = 'control-label'>Times per Year:</label></div>
-                                        <div class="col-sm-6"><input onkeypress='return isNumberKey(event);' class="form-control" id="times-per-year" placeholder="Times Per Year #"></div>
-                                        <div class="col-sm-6"><label class = 'control-label'>Cost per Time:</label></div>
-                                        <div class="col-sm-6"><input onkeypress='return isNumberKey(event);' class="form-control" id="cost-per-time" placeholder="Cost Per Time $"></div>
-                                        <div class="col-sm-6"><label class = 'control-label'>Yearly Estimate:</label></div>
-                                        <div class="col-sm-6"><input onkeypress='return isNumberKey(event);' class="form-control" id="yearly-estimate" placeholder="Yearly Estimate $"></div>
-                                    </div>
-                                </form>
-                            </div>
-                            <button type="button" id="nonMonthlySubmit" class="btn btn-primary pull-right">Submit</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div><!-- End Modal-->
-
     <script type = "text/javascript">
 
         $(function () {
@@ -221,7 +124,7 @@ function getCalculator($calcType, $id) {
                 placement: 'right',
                 container: 'body'
             });
-            
+
 
             $('#Income1').submit(function (e)
             {
@@ -235,20 +138,6 @@ function getCalculator($calcType, $id) {
             return !(charCode > 31 && (charCode < 48 || charCode > 57));
         }
 
-        $('#wageCalcModal').on('show.bs.modal', function (event) {
-            //var button = $(event.relatedTarget); // Button that triggered the modal
-            //var recipient = button.data('whatever'); // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            //var modal = $(this);
-            //modal.find('.modal-title').text('Wage Calculator');
-            //modal.find('.modal-body input').val(recipient);
-            //var id = $(event.relatedTarget).data('id');
-            //$(event.currentTarget).find('input[name="id"]').val(id);
-            //document.getElementById(id).innerHTML = (document.getElementById('dollars-per-hour').innerHTML).toFixed(2);
-        });
-        
-        //$('#wageCalcModal').modal(options);
     </script>
 
 </body>
