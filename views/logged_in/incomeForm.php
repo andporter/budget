e39d07631fd66bc887134da4c1e7ee84ac586b93
Braden Talbot <talbotbraden@gmail.com>
2015-07-19 14:40:55 -0600

    Fixed small bug (missing quote) in incomeForm and created accordionId variable to replace verbose $ResultsToReturn id

--- views/logged_in/incomeForm.php
+++ views/logged_in/incomeForm.php
@@ -5,23 +5,22 @@ include('GetDB.php');
 function getForm($CategoryParentType, $CategoryParentOrder)
 {
     $ResultsToReturn = getDB($CategoryParentType, $CategoryParentOrder);
+    $accordionId = $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"];
     ?>
     <div class="panel panel-primary">
-        <div class="panel-heading" role="tab" id="accordion<?php echo $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"]; ?>" >
-            <?php
-            echo '<h4 class="panel-title>';
-            echo '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#' . $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"] . '" aria-expanded="true" aria-controls="' . $ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"] . '" >';
-            echo $ResultsToReturn[0]["categoryParentOrder"] . ". " . $ResultsToReturn[0]["categoryParentName"];
-            echo '</a>';
-            echo '</h4>';
-            ?>
+        <div class="panel-heading" role="tab" id="accordion<?php echo $accordionId; ?>" >
+            <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $accordionId; ?>" aria-expanded="true" aria-controls="<?php echo $accordionId; ?>" >
+                    <?php echo $ResultsToReturn[0]["categoryParentOrder"] . ". " . $ResultsToReturn[0]["categoryParentName"]; ?>
+                </a>
+            </h4>
         </div>
-        <div id="<?php echo ($ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"]); ?>" class="panel-collapse collapse<?php
+        <div id="<?php echo $accordionId; ?>" class="panel-collapse collapse<?php
         if ($ResultsToReturn[0]["categoryParentOrder"] == 1 && $ResultsToReturn[0]["categoryParentName"] == "Monthly Earned Income")
         {
             echo ' in';
         }
-        ?>" role="tabpanel" aria-labelledby="accordion<?php echo ($ResultsToReturn[0]["categoryParentOrder"] . $ResultsToReturn[0]["categoryParentType"]); ?>" >
+        ?>" role="tabpanel" aria-labelledby="accordion<?php echo $accordionId; ?>" >
             <div class='panel-body'>
                 <form class='form-horizontal' role='form' id='<?php echo $CategoryParentType . $CategoryParentOrder ?>'>
                     <?php
@@ -73,28 +72,18 @@ function getForm($CategoryParentType, $CategoryParentOrder)
 <div class='container theme-showcase'>
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
         <?php
-        getForm("Income", 1);
-        getForm("Income", 2);
-        getForm("Income", 3);
-        getForm("Expense", 1);
-        getForm("Expense", 2);
-        getForm("Expense", 3);
-        getForm("Expense", 4);
-        getForm("Expense", 5);
-        getForm("Expense", 6);
-        getForm("Expense", 7);
-        getForm("Expense", 8);
-        getForm("Expense", 9);
+        for ($i = 1; $i <= 3; $i++)
+        {
+            getForm("Income", $i);
+        }
+        for ($i = 1; $i <= 9; $i++)
+        {
+            getForm("Expense", $i);
+        }
         ?>
     </div>
 </div>
 <script type = "text/javascript">
-    $(function () {
-        $("[rel=tooltip]").tooltip({
-            placement: 'right',
-            container: 'body'
-        });
-    });
     function submitForm(form, id) {
         var postJSONData = $(form).serializeArray();
         postJSONData = JSON.stringify(postJSONData);
