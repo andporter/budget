780410aa018fde51ef83b59ad2882c6b0148b632
Mark <Magesrook@gmail.com>
2015-07-19 14:35:44 -0600

    Merge origin/master
    Conflicts:
    	views/not_logged_in/initial_form.php

--- views/logged_in/demographicsForm.php
+++ views/logged_in/demographicsForm.php
@@ -9,45 +9,81 @@
 <div class='container theme-showcase'>
     <div class="panel panal-content panel-primary">
         <div class="panel-heading">
-            <h3 class="panel-title">User Information</h3>
+            <h3 class="panel-title">Demographic & Contact Information</h3>
         </div>
         <div class="panel-body">
 <!--            <div class="well">
             </div>-->
             <form class="form-horizontal" id="formContact" data-parsley-validate>
-                <div id="divFirstName" class="form-group">
-                    <label for="firstName" class="col-sm-2 control-label">First Name:</label>
+                
+                <div id="nameGroup" class="form-group">
+                    <label for="firstName" style="margin-left:15px; width: 100px" >First Name:</label>
+                    <input type="text" style="margin-left:15px" id="firstName" name="firstName" placeholder="First Name" data-parsley-group="firstName" required data-parsley-required-message="Please enter your First Name">
+                    <label for="lastName" style="margin-left:15px">Last Name:</label>
+                    <input type="text" style="margin-left:15px" id="lastName" name="lastName" placeholder="Last Name" data-parsley-group="lastName" required data-parsley-required-message="Please enter your Last Name">
+                </div>
+             
+                <div id="divPhone" class="form-group">
+                    <label for="phone" style="margin-left:15px; width: 100px">Phone:</label>
+                    <input type="text" style="margin-left:5px; width: 50px" name="phoneAreaCode" placeholder="###" data-parsley-group="phone" maxlength="3" size="3" data-parsley-length="[3, 3]" data-parsley-error-message="3 Digits Required"/>
+                    <input type="text" style="margin-left:5px; width: 50px" name="phoneFirstThree" placeholder="###" data-parsley-group="phone" maxlength="3" size="3" data-parsley-length="[3, 3]" data-parsley-error-message="3 Digits Required"/>
+                    <input type="text" style="margin-left:5px; width: 75px" name="phoneLastFour" placeholder="####" data-parsley-group="phone" maxlength="4" size="4" data-parsley-length="[4, 4]" data-parsley-error-message="4 Digits Required"/>
+                    
+                <label for="doYouText" style="margin-left:15px">Do You Text:</label>
+                <input type="checkbox" style="margin-left:15px">
+
+                </div>
+                <div class="invalid-form-error-message-require-emailORphone"></div>
+                
+                <div id="additionalPerson">
+                <label for="additionalPerson" class="col-sm-2 control-label">I Want to Add an Additional Person:</label>
                     <div class="col-xs-4">
-                        <input type="text" class="form-control" name="firstName" placeholder="First Name" data-parsley-group="firstName" required data-parsley-required-message="Please enter your First Name">
+                        <input type="checkbox" >
                     </div>
                 </div>
-                <div id="divLastName" class="form-group">
+                
+                <div id="divSecondFirstName" class="form-group">
+                    <label for="firstName" class="col-sm-2 control-label">First Name:</label>
+                       <div class="col-xs-4">
+                        <input type="text" >
+                    </div>
+                </div>
+
+                <div id="divSecondLastName" class="form-group">
                     <label for="lastName" class="col-sm-2 control-label">Last Name:</label>
                     <div class="col-xs-4">
-                        <input type="text" class="form-control" name="lastName" placeholder="Last Name" data-parsley-group="lastName" required data-parsley-required-message="Please enter your Last Name">
+                        <input type="text">
                     </div>
                 </div>
+                
+        
                 <div id="divEmail" class="form-group">
                     <label for="email" class="col-sm-2 control-label">Email:</label>
                     <div class="col-xs-4">
                         <input id="email" type="email" class="form-control" name="email" placeholder="Email" data-parsley-group="email"/>
                     </div>
                 </div>
-                <div id="divPhone" class="form-group">
-                    <label for="phone" class="col-sm-2 control-label">Phone:</label>
-                    <div class="col-xs-4" id="phone">
-                        <div class="col-xs-4">
-                            <input type="text" class="phonenumber form-control" name="phoneAreaCode" placeholder="###" data-parsley-group="phone" maxlength="3" size="3" data-parsley-length="[3, 3]" data-parsley-error-message="3 Digits Required"/>
-                        </div>
-                        <div class="col-xs-4">
-                            <input type="text" class="phonenumber form-control" name="phoneFirstThree" placeholder="###" data-parsley-group="phone" maxlength="3" size="3" data-parsley-length="[3, 3]" data-parsley-error-message="3 Digits Required"/>
-                        </div>
-                        <div class="col-xs-4">
-                            <input type="text" class="phonenumber form-control" name="phoneLastFour" placeholder="####" data-parsley-group="phone" maxlength="4" size="4" data-parsley-length="[4, 4]" data-parsley-error-message="4 Digits Required"/>
-                        </div>
+                
+                <div id="under4">
+                <label for="under4" class="col-sm-2 control-label">Number of Children 0-5 you support:</label>
+                    <div class="col-xs-4">
+                        <input type="text" >
                     </div>
                 </div>
-                <div class="invalid-form-error-message-require-emailORphone"></div>
+                
+                <div id="over5">
+                <label for="over5" class="col-sm-2 control-label">Number of Children 6-18 you support:</label>
+                    <div class="col-xs-4">
+                        <input type="text" >
+                    </div>
+                </div>
+    
+                <div id="totalHousehold">
+                <label for="totalHousehold" class="col-sm-2 control-label">Total number of household members you support:</label>
+                    <div class="col-xs-4">
+                        <input type="text" >
+                    </div>
+                </div>
 
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
