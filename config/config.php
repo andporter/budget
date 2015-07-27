732c5e416252183cbf03c4b8b27e0d4bdd7d4e1f
robbyhunsaker <robhunsaker3@gmail.com>
2015-07-26 23:12:58 -0600

    Merge origin/master
    Conflicts:
    	views/logged_in/incomeForm.php

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ define("DB_TYPE", "mysql");
 define("DB_HOST", "localhost");
 define("DB_NAME", "budget");
 define("DB_USER", "root");
-define("DB_PASS", "password");
+define("DB_PASS", "");
 
 // Define whether an HTTPS connection is required
 define("HTTPS_required", "FALSE");
