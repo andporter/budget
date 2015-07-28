bafbd69b4cc757e51ffee1f22e413ee1b445ba5e
Braden Talbot <talbotbraden@gmail.com>
2015-07-27 21:01:01 -0600

    budgetReview Works

--- views/logged_in/review.php
+++ views/logged_in/review.php
@@ -1,13 +1,26 @@
 <?php
+
 $grossIncome = 0.0;
+$totalExpense = 0.0;
+$subtotal = 0.0;
+$total = 0.0;
+$net = 0.0;
+
 include'getDB.php';
+
+// get gross income for all reviews
+for ($i = 1; $i <= 3; $i++)
+{
+    $ResultsToReturn = getDB('Income', $i);
+    foreach ($ResultsToReturn as $row)
+    {
+        $grossIncome += $row["budgetSelfAmount"] + $row["budgetSpouseAmount"];
+    }
+}
+
+echo '<div id="review">';
 //include 'incomeReview.php';
 //include 'expenseReview.php';
 include'budgetReview.php';
+echo '</div>';
 ?>
-<div id="review">
-
-</div>
-<div class="panel-body">
-    <input type="button" value="Next" class="btn btn-primary pull-right" onclick="" id="nextReview">
-</div>
