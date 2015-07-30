319f7fecb87157bc56524b7f94fdfd279307544f
Mark <Magesrook@gmail.com>
2015-07-29 19:28:50 -0600

    Merge origin/master
    Conflicts:
    	views/not_logged_in/initial_form.php

--- views/logged_in/demographicsForm.php
+++ views/logged_in/demographicsForm.php
@@ -17,57 +17,62 @@ if ($sql->execute())
             <h3 class="panel-title">Demographic & Contact Information</h3>
         </div>
         <div class="panel-body">
-            <form class="form-horizontal" id="formDemographics" data-parsley-validate>
+            <form class="form-horizontal" id="formDemographics">
 
                 <div id="nameGroup" class="form-group">
                     <label for="firstName" style="margin-left:15px; width: 100px" >First Name:</label>
-                    <input type="text" style="margin-left:30px; width: 195px" id="firstName" name="firstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["firstName"] ?>" data-parsley-group="firstName" required data-parsley-required-message="Please enter your First Name">
+                    <input type="text" style="margin-left:30px; width: 195px" id="firstName" name="firstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["firstName"] ?>" required>                    
                     <label for="lastName" style="margin-left:15px; width: 100px">Last Name:</label>
-                    <input type="text" style="margin-left:30px; width: 175" id="lastName" name="lastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["lastName"] ?>" data-parsley-group="lastName" required data-parsley-required-message="Please enter your Last Name">
+                    <input type="text" style="margin-left:30px; width: 175" id="lastName" name="lastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["lastName"] ?>" required>
+                    <div class="errors"><div id="firstNameError" class="errors" value=""></div><div id="lastNameError" class="errors" value=""></div></div>
                 </div>
+                
 
                 <div id="divPhone" class="form-group">
-                    <label for="phone" style="margin-left:15px; width: 125px">Phone:</label>
-                    <input type="text" style="margin-left:5px; width: 50px" name="phoneAreaCode" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 0, 3) ?>" data-parsley-group="phone" maxlength="3" size="3" data-parsley-length="[3, 3]" data-parsley-error-message="3 Digits Required"/>
-                    <input type="text" style="margin-left:5px; width: 50px" name="phoneFirstThree" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 4, 3) ?>" data-parsley-group="phone" maxlength="3" size="3" data-parsley-length="[3, 3]" data-parsley-error-message="3 Digits Required"/>
-                    <input type="text" style="margin-left:5px; width: 75px" name="phoneLastFour" onkeypress='return isNumberKey(event);' placeholder="####" value="<?php echo substr($ResultsToReturn[0]["phone"], 8, 4) ?>" data-parsley-group="phone" maxlength="4" size="4" data-parsley-length="[4, 4]" data-parsley-error-message="4 Digits Required"/>
-
-                    <label for="doYouText" style="margin-left:15px">Do You Text:</label>
+                    <label for="phone" name="phone" style="margin-left:15px; width: 125px">Phone:</label>
+                    <input type="text" id="phoneAreaCode" style="margin-left:5px; width: 50px" name="phoneAreaCode" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 0, 3) ?>" required/>
+                    <input type="text" id="phoneFirstThree" style="margin-left:5px; width: 50px" name="phoneFirstThree" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 4, 3) ?>" required/>
+                    <input type="text" id="phoneLastFour" style="margin-left:5px; width: 75px" name="phoneLastFour" onkeypress='return isNumberKey(event);' placeholder="####" value="<?php echo substr($ResultsToReturn[0]["phone"], 8, 4) ?>" required/>
+                    <label for="doYouText" id="doYouText" name="doYouText" style="margin-left:15px">Do You Text:</label>
                     <input id="phoneCanText" name="phoneCanText" type="checkbox" <?php if ($ResultsToReturn[0]["phoneCanText"] == "0"){echo "checked";} ?> style="margin-left:45px">
-
+                    <div class="errors"><div id="phoneAreaCodeError" class="errors" value=""></div><div id="phoneFirstThreeError" class="errors" value=""></div><div id="phoneLastFourError" class="errors" value=""></div></div>
                 </div>
-                <div class="invalid-form-error-message-require-emailORphone"></div>
 
-                <div id="additional"class="form-group">
-                    <label for="additionalPerson" style="margin-left:15px">I'm married (Have someone living in the home that I want to include)</label>
+                <div id="additional" class="form-group">
+                    <label for="additionalPerson" name="additionalPerson" style="margin-left:15px">I'm married (Have someone living in the home that I want to include)</label>
                     <input id="isMarried" name="isMarried" type="checkbox" <?php if ($ResultsToReturn[0]["isMarried"] == "0"){echo "checked";} ?> style="margin-left:105px">
                 </div>
 
                 <div id="secondNameGroup" class="form-group">
-                    <label for="spouseFirstName" style="margin-left:15px; width: 100px" >First Name:</label>
-                    <input type="text" style="margin-left:30px; width: 195px" id="spouseFirstName" name="spouseFirstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["spouseFirstName"] ?>" data-parsley-group="firstName" required data-parsley-required-message="Please enter your First Name">
-                    <label for="spouseLastName" style="margin-left:15px; width: 100px">Last Name:</label>
-                    <input type="text" style="margin-left:30px; width: 195px" id="spouseLastName" name="spouseLastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["spouseLastName"] ?>" data-parsley-group="lastName" required data-parsley-required-message="Please enter your Last Name">
+                    <label for="spouseFirstName" name="spouseFirstName" style="margin-left:15px; width: 100px" >First Name:</label>
+                    <input type="text" style="margin-left:30px; width: 195px" id="spouseFirstName" name="spouseFirstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["spouseFirstName"] ?>" >
+                    <label for="spouseLastName" name="spouseLastName" style="margin-left:15px; width: 100px">Last Name:</label>
+                    <input type="text" style="margin-left:30px; width: 195px" id="spouseLastName" name="spouseLastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["spouseLastName"] ?>" >
+                    <div><div id="spouseEmailError" class="errors" value="" hidden="true"></div><div id="spouseFirstNameError" class="errors" value="" hidden="true"></div><div id="spouseLastNameError" class="errors" value="" hidden="true"></div></div>
                 </div>
 
                 <div id="divEmail" class="form-group">
-                    <label for="spouseEmail" style="margin-left:15px; width: 100px">Email:</label>
+                    <label for="spouseEmail" name="spouseEmail" style="margin-left:15px; width: 100px">Email:</label>
                     <input id="spouseEmail" style="margin-left:30px; width: 300px" type="email" name="spouseEmail" placeholder="Email" value="<?php echo $ResultsToReturn[0]["spouseEmail"] ?>" data-parsley-group="email"/>
+                    <div><div id="dependent0_4Error" class="errors" value=""></div></div>
                 </div>
 
                 <div id="under4" class="form-group">
-                    <label for="dependent0_4" style="margin-left:15px; width: 400px" >Number of Children 0-4 you support:</label>
-                    <input id="dependent0_4" name="dependent0_4" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent0_4"] ?>" style="margin-left:15px">
+                    <label for="dependent0_4" name="dependent0_4" style="margin-left:15px; width: 400px" >Number of Children 0-4 you support:</label>
+                    <input id="dependent0_4" name="dependent0_4" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent0_4"] ?>" style="margin-left:15px" required>
+                    <div><div id="dependent0_4Error" class="errors" value=""></div></div>
                 </div>
 
                 <div id="over5" class="form-group">
-                    <label for="dependent5_18" style="margin-left:15px; width: 400px">Number of Children 5-18 you support:</label>
-                    <input id="dependent5_18" name="dependent5_18" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent5_18"] ?>" style="margin-left:15px">
+                    <label for="dependent5_18" name="dependent5_18" style="margin-left:15px; width: 400px">Number of Children 5-18 you support:</label>
+                    <input id="dependent5_18" name="dependent5_18" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent5_18"] ?>" style="margin-left:15px" required>
+                    <div><div id="dependent5_18Error" class="errors" value=""></div></div>
                 </div>
 
                 <div id="dependentAdditional" class="form-group">
-                    <label for="dependentAdditional" style="margin-left:15px; width: 400px">Additional household member you support (including yourself and spouse):</label>
-                    <input id="dependentAdditional" name="dependentAdditional" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependentAdditional"] ?>" style="margin-left:15px">
+                    <label for="dependentAdditional" name="dependentAdditional" style="margin-left:15px; width: 400px">Additional household member you support (including yourself and spouse):</label>
+                    <input id="dependentAdditional" name="dependentAdditional" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependentAdditional"] ?>" style="margin-left:15px" required>
+                    <div><div id="dependentAdditionalError" value=""></div></div>
                 </div>
 
                 <div class="form-group">
@@ -83,11 +88,26 @@ if ($sql->execute())
 <script type = "text/javascript">    
     $('#buttonSave_formDemographics').on('click', function ()
     {
+ 
         var btn = $(this).button('loading');
         
         var postJSONData = $('#formDemographics').serializeObject();
+        
+        
+        for(var x in postJSONData)
+        {
+                errorId = (x + "Error");
+                if (postJSONData[x].toString() == null || postJSONData[x].toString() == "") 
+                {
+                    document.getElementById(errorId).innerHTML =(x + " must be filled out");
+                }
+                else
+                {
+                    document.getElementById(errorId).innerHTML = "";
+                }
+        }
+        
         postJSONData = JSON.stringify(postJSONData);
-
         SendAjax("api/api.php?method=userDemographicsFormSubmit", postJSONData, "none", true);
         
         setTimeout(function () {
