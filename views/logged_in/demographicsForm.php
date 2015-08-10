e5744c4e28b348f55aa1f7dbdef65d67c82d9780
Mark <Magesrook@gmail.com>
2015-08-09 18:07:29 -0600

    Merge origin/master
    Conflicts:
    	views/not_logged_in/initial_form.php

--- views/logged_in/demographicsForm.php
+++ views/logged_in/demographicsForm.php
@@ -20,58 +20,60 @@ if ($sql->execute())
             <form class="form-horizontal" id="formDemographics">
 
                 <div id="nameGroup" class="form-group">
-                    <label for="firstName" style="margin-left:15px; width: 100px" >First Name:</label>
-                    <input type="text" style="margin-left:30px; width: 195px" id="firstName" name="firstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["firstName"] ?>" required>                    
-                    <label for="lastName" style="margin-left:15px; width: 100px">Last Name:</label>
-                    <input type="text" style="margin-left:30px; width: 175" id="lastName" name="lastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["lastName"] ?>" required>
+                    <label for="firstName" class="col1" >First Name:</label>
+                    <input type="text" class="col2" id="firstName" name="firstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["firstName"] ?>" required>                    
+                    <label for="lastName" class="col2">Last Name:</label>
+                    <input type="text" class="col2" id="lastName" name="lastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["lastName"] ?>" required>
                     <div class="errors"><div id="firstNameError" class="errors" value=""></div><div id="lastNameError" class="errors" value=""></div></div>
                 </div>
                 
 
                 <div id="divPhone" class="form-group">
-                    <label for="phone" name="phone" style="margin-left:15px; width: 125px">Phone:</label>
-                    <input type="text" id="phoneAreaCode" style="margin-left:5px; width: 50px" name="phoneAreaCode" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 0, 3) ?>" required/>
-                    <input type="text" id="phoneFirstThree" style="margin-left:5px; width: 50px" name="phoneFirstThree" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 4, 3) ?>" required/>
-                    <input type="text" id="phoneLastFour" style="margin-left:5px; width: 75px" name="phoneLastFour" onkeypress='return isNumberKey(event);' placeholder="####" value="<?php echo substr($ResultsToReturn[0]["phone"], 8, 4) ?>" required/>
-                    <label for="doYouText" id="doYouText" name="doYouText" style="margin-left:15px">Do You Text:</label>
-                    <input id="phoneCanText" name="phoneCanText" type="checkbox" <?php if ($ResultsToReturn[0]["phoneCanText"] == "0"){echo "checked";} ?> style="margin-left:45px">
+                    <label for="phone" name="phone" class="col1">Phone:</label>
+                    <div class="colphone">
+                    <input type="text" id="phoneAreaCode"  class="firstSet" name="phoneAreaCode" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 0, 3) ?>" required/>
+                    <input type="text" id="phoneFirstThree" class="secondSet" name="phoneFirstThree" onkeypress='return isNumberKey(event);' placeholder="###" value="<?php echo substr($ResultsToReturn[0]["phone"], 4, 3) ?>" required/>
+                    <input type="text" id="phoneLastFour" class="thirdSet" name="phoneLastFour" onkeypress='return isNumberKey(event);' placeholder="####" value="<?php echo substr($ResultsToReturn[0]["phone"], 8, 4) ?>" required/>
+                    </div>
+                    <label for="doYouText" id="doYouText" name="doYouText" class="col2">Do You Text:</label>
+                    <input id="phoneCanText" name="phoneCanText" class="col4" type="checkbox" <?php if ($ResultsToReturn[0]["phoneCanText"] == "0"){echo "checked";} ?> style="margin-left:45px">
                     <div class="errors"><div id="phoneAreaCodeError" class="errors" value=""></div><div id="phoneFirstThreeError" class="errors" value=""></div><div id="phoneLastFourError" class="errors" value=""></div></div>
                 </div>
 
                 <div id="additional" class="form-group">
-                    <label for="additionalPerson" name="additionalPerson" style="margin-left:15px">I'm married (Have someone living in the home that I want to include)</label>
-                    <input id="isMarried" name="isMarried" type="checkbox" <?php if ($ResultsToReturn[0]["isMarried"] == "0"){echo "checked";} ?> style="margin-left:105px">
+                    <label for="additionalPerson" name="additionalPerson" class="col5">I'm married (Have someone living in the home that I want to include)</label>
+                    <input id="isMarried" name="isMarried" class="col4" type="checkbox" <?php if ($ResultsToReturn[0]["isMarried"] == "0"){echo "checked";} ?> style="margin-left:105px">
                 </div>
 
                 <div id="secondNameGroup" class="form-group">
-                    <label for="spouseFirstName" name="spouseFirstName" style="margin-left:15px; width: 100px" >First Name:</label>
-                    <input type="text" style="margin-left:30px; width: 195px" id="spouseFirstName" name="spouseFirstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["spouseFirstName"] ?>" >
-                    <label for="spouseLastName" name="spouseLastName" style="margin-left:15px; width: 100px">Last Name:</label>
-                    <input type="text" style="margin-left:30px; width: 195px" id="spouseLastName" name="spouseLastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["spouseLastName"] ?>" >
+                    <label for="spouseFirstName" name="spouseFirstName" class="col1" width: 100px" >First Name:</label>
+                    <input type="text" class="col2" id="spouseFirstName" name="spouseFirstName" placeholder="First Name" value="<?php echo $ResultsToReturn[0]["spouseFirstName"] ?>" >
+                    <label for="spouseLastName" name="spouseLastName" class="col2">Last Name:</label>
+                    <input type="text" class="col2" id="spouseLastName" name="spouseLastName" placeholder="Last Name" value="<?php echo $ResultsToReturn[0]["spouseLastName"] ?>" >
                     <div><div id="spouseEmailError" class="errors" value="" hidden="true"></div><div id="spouseFirstNameError" class="errors" value="" hidden="true"></div><div id="spouseLastNameError" class="errors" value="" hidden="true"></div></div>
                 </div>
 
                 <div id="divEmail" class="form-group">
-                    <label for="spouseEmail" name="spouseEmail" style="margin-left:15px; width: 100px">Email:</label>
-                    <input id="spouseEmail" style="margin-left:30px; width: 300px" type="email" name="spouseEmail" placeholder="Email" value="<?php echo $ResultsToReturn[0]["spouseEmail"] ?>" data-parsley-group="email"/>
+                    <label for="spouseEmail" class="col3" name="spouseEmail" style="margin-left:15px; width: 100px">Email:</label>
+                    <input id="spouseEmail" class="" type="email" name="spouseEmail" placeholder="Email" value="<?php echo $ResultsToReturn[0]["spouseEmail"] ?>" data-parsley-group="email"/>
                     <div><div id="spouseEmailError" class="errors" value=""></div></div>
                 </div>
 
                 <div id="under4" class="form-group">
-                    <label for="dependent0_4" name="dependent0_4" style="margin-left:15px; width: 400px" >Number of Children 0-4 you support:</label>
-                    <input id="dependent0_4" name="dependent0_4" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent0_4"] ?>" style="margin-left:15px" required>
+                    <label for="dependent0_4" name="dependent0_4" class="col3" >Number of Children 0-4 you support:</label>
+                    <input id="dependent0_4" name="dependent0_4" class="col4" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent0_4"] ?>" style="margin-left:15px" required>
                     <div><div id="dependent0_4Error" class="errors" value=""></div></div>
                 </div>
 
                 <div id="over5" class="form-group">
-                    <label for="dependent5_18" name="dependent5_18" style="margin-left:15px; width: 400px">Number of Children 5-18 you support:</label>
-                    <input id="dependent5_18" name="dependent5_18" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent5_18"] ?>" style="margin-left:15px" required>
+                    <label for="dependent5_18" name="dependent5_18" class="col3" >Number of Children 5-18 you support:</label>
+                    <input id="dependent5_18" name="dependent5_18" class="col4" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependent5_18"] ?>" style="margin-left:15px" required>
                     <div><div id="dependent5_18Error" class="errors" value=""></div></div>
                 </div>
 
                 <div id="dependentAdditional" class="form-group">
-                    <label for="dependentAdditional" name="dependentAdditional" style="margin-left:15px; width: 400px">Additional household member you support (including yourself and spouse):</label>
-                    <input id="dependentAdditional" name="dependentAdditional" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependentAdditional"] ?>" style="margin-left:15px" required>
+                    <label for="dependentAdditional" name="dependentAdditional" class="col3" >Additional household member you support (including yourself and spouse):</label>
+                    <input id="dependentAdditional" name="dependentAdditional" class="col4" type="text" onkeypress='return isNumberKey(event);' value="<?php echo $ResultsToReturn[0]["dependentAdditional"] ?>" style="margin-left:15px" required>
                     <div><div id="dependentAdditionalError" class="errors"  value=""></div></div>
                 </div>
 
